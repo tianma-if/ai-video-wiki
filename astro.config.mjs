bdde6e60764f468fa736445303eabeec4c825b5e
yoma <yingwaizhiying@gmail.com>
2026-07-11 17:48:45 +0800

    feat: override Starlight Header component to render custom top navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,9 @@ export default defineConfig({
       customCss: [
         './src/styles/custom.css',
       ],
+      components: {
+        Header: './src/components/StarlightHeader.astro',
+      },
       sidebar: [
         {
           label: '速查分类',
